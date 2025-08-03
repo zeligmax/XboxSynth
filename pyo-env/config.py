1499eaa0abc94af9cf7901794ba8b8f29d52a700
Max Montlleo <zeligmax@gmail.com>
2025-08-03 20:33:17 +0200

    Update config.py

--- pyo-env/config.py
+++ pyo-env/config.py
@@ -1,9 +1,17 @@
 # config.py
 tables = ['Saw', 'Square', 'Harm']
+
 note_map = {
-    0: 0,  # A
-    1: 2,  # B
-    2: 4,  # X
-    3: 5,  # Y
+    0: 6,  # A → SI
+    1: 9,  # B → LA
+    2: 5,  # X → FA
+    3: 7,  # Y → SOL
 }
+
+hat_map = {
+    (-1, 0): 0,  # izquierda → DO
+    (0, 1): 2,   # arriba → RE
+    (1, 0): 4,   # derecha → MI
+}
+
 base_freq = 220
